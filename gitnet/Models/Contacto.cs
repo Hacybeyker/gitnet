4e892ec944ac4a7692e0c4ce379fd32da6013ace
hacybeyker@gmail.com <hacybeyker@gmail.com>
2016-04-05 17:25:33 +0000

    agrege la clase contacto

---
+++ gitnet/Models/Contacto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gitnet.Models
+{
+    public class Contacto
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string mensaje { get; set; }
+    }
+}
